d00e488c6636d7acf22c3f44d7bc7d42ababefd5
LGD_Sunday <1205507971@qq.com>
2020-11-30 15:12:29 +0800

    feat：detail desc

--- src/views/house_detail/Main.vue
+++ src/views/house_detail/Main.vue
@@ -5,6 +5,8 @@
       <house-detail-nav-bar :title="detailData.name"></house-detail-nav-bar>
       <!-- swiper -->
       <house-detail-swiper :data="detailData"></house-detail-swiper>
+      <!-- 描述文本 -->
+      <house-detail-desc :data="detailData"></house-detail-desc>
     </div>
   </div>
 </template>
@@ -12,12 +14,14 @@
 <script>
 import HouseDetailNavBar from './components/HouseDetailNavBar.vue';
 import HouseDetailSwiper from './components/HouseDetailSwiper.vue';
+import HouseDetailDesc from './components/HouseDetailDesc.vue';
 import { getHouse } from '/@api/house';
 export default {
   name: 'HouseDetail',
   components: {
     HouseDetailNavBar,
-    HouseDetailSwiper
+    HouseDetailSwiper,
+    HouseDetailDesc
   },
   data() {
     return {
---
+++ src/views/house_detail/components/HouseDetailDesc.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="house-detail-desc">
+    <p class="house-detail-desc-name">{{ data.name }}</p>
+    <p class="house-detail-desc-price">{{ data.price }} <span>元/㎡</span></p>
+    <p class="house-detail-desc-tags">
+      <van-tag v-for="tag in data.tags" :key="tag" type="success">
+        {{ tag }}
+      </van-tag>
+    </p>
+    <p class="house-detail-desc-area">建面 &nbsp; {{ data.minArea }}-{{ data.maxArea }}㎡</p>
+    <p class="house-detail-desc-address">地址 &nbsp; {{ data.address }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HouseDetailDesc',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'src/assets/css/constant.scss';
+.house-detail-desc {
+  background-color: white;
+  padding: $margin;
+  font-size: 14px;
+  p {
+    margin-bottom: 6px;
+  }
+  &-name {
+    font-size: 18px;
+    font-weight: 600;
+  }
+  &-price {
+    font-size: 20px;
+    font-weight: 600;
+    color: #fd503c;
+    span {
+      font-size: 12px;
+    }
+  }
+  &-tags {
+    .van-tag {
+      margin-right: 6px;
+    }
+  }
+}
+</style>
